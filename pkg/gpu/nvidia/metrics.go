2d13d4cb7032206aeb85c8321105b74f9915ea6a
ghokun <gokhun@gmail.com>
2022-03-21 17:52:49 +0300

    Testing previous config v1alpha2
    Signed-off-by: ghokun <gokhun@gmail.com>

--- pkg/gpu/nvidia/metrics.go
+++ pkg/gpu/nvidia/metrics.go
@@ -16,7 +16,7 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	pb "k8s.io/cri-api/pkg/apis/runtime/v1"
+	pb "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
 )
 
 const (
